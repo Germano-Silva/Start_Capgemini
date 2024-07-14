7d5da572d9dd7a734449c0e76be47b5539c7b73b
Germano-Silva <developer.germano.silva@gmail.com>
2024-07-14 20:04:46 -0300

    Aula 4.C 01

--- Start By Capgemini/02_Trilha_Introdutoria_LOGICA/04_Logica_de_Programacao_e_Algoritmos_3/aulas_java/src/aulas/Aula_4_C_01.java
+++ Start By Capgemini/02_Trilha_Introdutoria_LOGICA/04_Logica_de_Programacao_e_Algoritmos_3/aulas_java/src/aulas/Aula_4_C_01.java
@@ -1,5 +1,9 @@
-public class App {
+package aulas;
+
+public class Aula_4_C_01 {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        System.out.println("Meu segundo comando de saida!");
     }
+
 }
