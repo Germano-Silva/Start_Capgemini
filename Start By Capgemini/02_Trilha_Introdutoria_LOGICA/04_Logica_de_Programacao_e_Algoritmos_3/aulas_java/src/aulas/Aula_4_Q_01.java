14de052b705a277851f312d3cbabfd81bec3ba4e
Germano-Silva <developer.germano.silva@gmail.com>
2024-07-16 20:49:37 -0300

    Aula 4.Q

---
+++ Start By Capgemini/02_Trilha_Introdutoria_LOGICA/04_Logica_de_Programacao_e_Algoritmos_3/aulas_java/src/aulas/Aula_4_Q_01.java
@@ -0,0 +1,41 @@
+package aulas;
+
+public class Aula_4_Q_01 {
+/**
+ *  A programação estruturada é um paradigma de programação que busca 
+ *  organizar o código em estruturas lógicas bem definidas, 
+ *  como sequência, seleção e repetição. É baseada no princípio do "top-down" ou "descendente",
+ *  em que o programa é dividido em módulos menores e mais simples, facilitando a compreensão,
+ *  manutenção e reutilização do código. A programação estruturada utiliza principalmente as
+ *  estruturas de controle condicional (como if-else) e de repetição (como while e for) 
+ *  para controlar o fluxo de execução do programa.
+ * 
+ *  Programação orientada a objetos (POO) é um paradigma de programação que organiza o código
+ *  em torno de objetos, que são instâncias de classes. Um objeto é uma entidade que possui
+ *  características (atributos) e comportamentos (métodos).
+ * 
+ *  Na programação orientada a objetos, o foco está na modelagem do mundo real,
+ *  onde os objetos representam entidades reais ou abstratas. 
+ *  As classes são as estruturas que definem os objetos, descrevendo seus atributos e métodos.
+ * 
+ *  Os princípios fundamentais da programação orientada a objetos são:
+ * 
+ * => Encapsulamento: a capacidade de ocultar detalhes internos do objeto 
+ *   e expor apenas o necessário para sua interação.
+ * 
+ * => Herança: a capacidade de criar novas classes baseadas em classes existentes,
+ *   herdam atributos e comportamentos da classe pai.
+ * 
+ * => Polimorfismo: a capacidade de um objeto se comportar de maneiras diferentes,
+ *   dependendo do contexto em que é usado.
+ * 
+ * => Abstração: a capacidade de representar entidades complexas do mundo real como
+ *   objetos simplificados em software.
+ * 
+ *  A programação orientada a objetos permite criar programas mais modulares,
+ *  flexíveis, reutilizáveis e de fácil manutenção.
+ * 
+ */
+
+
+}
