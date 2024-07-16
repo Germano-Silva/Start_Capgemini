386cafdef7c130f69c8b4181f2c7edf2a8b33c6a
Germano-Silva <developer.germano.silva@gmail.com>
2024-07-15 23:51:09 -0300

    Lista 1 - Exercício 01

--- Start By Capgemini/02_Trilha_Introdutoria_LOGICA/04_Logica_de_Programacao_e_Algoritmos_3/aulas_java/src/listas/lista_01/Exercicio_01.java
+++ Start By Capgemini/02_Trilha_Introdutoria_LOGICA/04_Logica_de_Programacao_e_Algoritmos_3/aulas_java/src/listas/lista_01/Exercicio_01.java
@@ -1,5 +1,8 @@
 package listas.lista_01;
 
+import java.io.IOException;
+import java.util.Scanner;
+
 /**
  * Códigos realizados por Germano Silva.
  * GitHub: { @literal https://github.com/Germano-Silva}
@@ -14,6 +17,29 @@ package listas.lista_01;
   */
 
 public class Exercicio_01 {
+  public static void main(String[] args) throws Exception {
+    clearScreen();
+    Scanner leitor = new Scanner(System.in);
+
+    double n1, n2, n3;
+
+    System.out.println("Informe um número!");
+    n1 = leitor.nextDouble();
+    System.out.println("Informe mais um número!");
+    n2 = leitor.nextDouble();
+    n3 = n1 + n2;
+    System.out.println("A soma dos número é " + n3 );
+    
+    leitor.close();
+  }
+
+  public static void clearScreen() throws IOException, InterruptedException {
+    try {
+      new ProcessBuilder("cmd", "/c", "cls", "clear").inheritIO().start().waitFor();
+    } catch (Exception e) {
+      System.err.println("Erro ao limpar o terminal: " + e.getMessage());
+    }
+  }
 
 
 }
