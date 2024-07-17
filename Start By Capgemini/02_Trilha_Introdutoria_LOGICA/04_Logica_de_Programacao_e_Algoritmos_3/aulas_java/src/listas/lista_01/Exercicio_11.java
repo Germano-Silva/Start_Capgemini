7ae2b82ca76dc02af25242c8eaa8aa022e0498df
developer.germano.silva <developer.germano.silva@gmail.com>
2024-07-17 17:51:26 +0000

    Exercício 11

--- Start By Capgemini/02_Trilha_Introdutoria_LOGICA/04_Logica_de_Programacao_e_Algoritmos_3/aulas_java/src/listas/lista_01/Exercicio_11.java
+++ Start By Capgemini/02_Trilha_Introdutoria_LOGICA/04_Logica_de_Programacao_e_Algoritmos_3/aulas_java/src/listas/lista_01/Exercicio_11.java
@@ -11,8 +11,29 @@ package listas.lista_01;
  * Exercício: 11.
  * 
  * Faça um algoritmo que receba o preço de custo de um produto e mostre o valor de venda.
- * Sabe-se que o preço de custo receberá um acréscimo de acordo com um percentual informado pelo usuário
+ * Sabe-se que o preço de custo receberá um acréscimo de acordo com um percentual informado 
+ * pelo usuário
  */
 public class Exercicio_11 {
+    public static void main(String[] args) throws Exception {
+        Scanner leitor = new Scanner(System.in);
+
+        System.out.print("Informe o preço de custo do produto: R$ ");
+        double precoCusto = leitor.nextDouble();
+        System.out.print("Informe o percentual de acréscimo: ");
+        double percentualAcrescimo = leitor.nextDouble();
+        double valorVenda = precoCusto * (1 + percentualAcrescimo / 100);
+        System.out.println("O valor de venda do produto é: R$ " + valorVenda);
+        leitor.close();
+      
+    }
+    
+      public static void clearScreen() throws IOException, InterruptedException {
+        try {
+          new ProcessBuilder("cmd", "/c", "cls", "clear").inheritIO().start().waitFor();
+          } catch (Exception e) {
+            System.err.println("Erro ao limpar o terminal: " + e.getMessage());
+          }
+      }
 
 }
