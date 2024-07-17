b96f8c220d4c4e0280f47aa56b283a12508f632b
Germano-Silva <developer.germano.silva@gmail.com>
2024-07-16 21:05:37 -0300

    Exercicio 02

--- Start By Capgemini/02_Trilha_Introdutoria_LOGICA/04_Logica_de_Programacao_e_Algoritmos_3/aulas_java/src/listas/lista_01/Exercicio_02.java
+++ Start By Capgemini/02_Trilha_Introdutoria_LOGICA/04_Logica_de_Programacao_e_Algoritmos_3/aulas_java/src/listas/lista_01/Exercicio_02.java
@@ -1,5 +1,8 @@
 package listas.lista_01;
 
+import java.io.IOException;
+import java.util.Scanner;
+
 /**
  * Códigos realizados por Germano Silva.
  * GitHub: {@literal https://github.com/Germano-Silva}
@@ -15,5 +18,35 @@ package listas.lista_01;
   */
 
 public class Exercicio_02 {
+  public static void main(String[] args) throws Exception {
+    clearScreen();
+    Scanner leitor = new Scanner(System.in);
+
+    double n1, n2, soma, multiplica, sub, div;
+
+    System.out.println("Informe um número!");
+    n1 = leitor.nextDouble();
+    System.out.println("Informe mais um número!");
+    n2 = leitor.nextDouble();
+    soma = n1 + n2;
+    System.out.println("A soma dos número é " + soma );
+    multiplica = n1 * n2;
+    System.out.println("A soma dos número é " + multiplica );
+    sub = n1 - n2;
+    System.out.println("A soma dos número é " + sub );
+    div = n1 / n2;
+    System.out.println("A soma dos número é " + div );
+    leitor.close();
+
+    
+  }
+
+  public static void clearScreen() throws IOException, InterruptedException {
+    try {
+      new ProcessBuilder("cmd", "/c", "cls", "clear").inheritIO().start().waitFor();
+    } catch (Exception e) {
+      System.err.println("Erro ao limpar o terminal: " + e.getMessage());
+    }
+  }
 
 }
