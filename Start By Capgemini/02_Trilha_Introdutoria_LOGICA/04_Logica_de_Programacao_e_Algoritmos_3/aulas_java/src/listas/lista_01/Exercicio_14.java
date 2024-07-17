1ae7a43c8b7076061e9b6f7637c26a6d2e64a2fb
Germano-Silva <developer.germano.silva@gmail.com>
2024-07-17 20:11:38 -0300

    Exercício 14

--- Start By Capgemini/02_Trilha_Introdutoria_LOGICA/04_Logica_de_Programacao_e_Algoritmos_3/aulas_java/src/listas/lista_01/Exercicio_14.java
+++ Start By Capgemini/02_Trilha_Introdutoria_LOGICA/04_Logica_de_Programacao_e_Algoritmos_3/aulas_java/src/listas/lista_01/Exercicio_14.java
@@ -1,5 +1,8 @@
 package listas.lista_01;
 
+import java.io.IOException;
+import java.util.Scanner;
+
 /**
  * Códigos realizados por Germano Silva.
  * GitHub: {@literal https://github.com/Germano-Silva}
@@ -13,5 +16,29 @@ package listas.lista_01;
  * Escreva um algoritmo que leia dois valores inteiro distintos e informe qual é o maior.
  */
 public class Exercicio_14 {
+    public static void main(String[] args) throws Exception {
+        clearScreen();
+        Scanner leitor = new Scanner(System.in);
+        System.out.println("Informe o primeiro valor: "); 
+        int numero1 = leitor.nextInt();
+        System.out.println("Informe o segundo valor: ");
+        int numero2 = leitor.nextInt();
+        
+        if (numero1>numero2) {
+            System.out.println("O primeiro valor é maior que o segundo!");
+        } else {
+            System.out.println("O Segundo valor é maior que o primeiro!");
+        } 
+        leitor.close();
+        
+    }
+
+    public static void clearScreen() throws IOException, InterruptedException {
+        try {
+            new ProcessBuilder("cmd", "/c", "cls", "clear").inheritIO().start().waitFor();
+        } catch (Exception e) {
+            System.err.println("Erro ao limpar o terminal: " + e.getMessage());
+        }
+    }
 
 }
