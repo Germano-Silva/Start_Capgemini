71371ba2116b7aa487ba1da5fc8dab22899dca93
Germano-Silva <developer.germano.silva@gmail.com>
2024-07-17 20:31:07 -0300

    Exercício 16

--- Start By Capgemini/02_Trilha_Introdutoria_LOGICA/04_Logica_de_Programacao_e_Algoritmos_3/aulas_java/src/listas/lista_01/Exercicio_16.java
+++ Start By Capgemini/02_Trilha_Introdutoria_LOGICA/04_Logica_de_Programacao_e_Algoritmos_3/aulas_java/src/listas/lista_01/Exercicio_16.java
@@ -1,5 +1,8 @@
 package listas.lista_01;
 
+import java.io.IOException;
+import java.util.Scanner;
+
 /**
  * Códigos realizados por Germano Silva.
  * GitHub: {@literal https://github.com/Germano-Silva}
@@ -16,5 +19,36 @@ package listas.lista_01;
   */
   
 public class Exercicio_16 {
+  public static void main(String[] args) throws Exception {
+    clearScreen();
+    Scanner leitor = new Scanner(System.in);
+    System.out.print("Informe sue nome: ");
+    String nome = leitor.nextLine();
+    System.out.println("Informe a sua 1ª nota: ");
+    double nota1 = leitor.nextDouble();
+    System.out.println("Informe a sua 2ª nota: ");
+    double nota2 = leitor.nextDouble();
+    System.out.println("Informe a sua 3ª nota: ");
+    double nota3 = leitor.nextDouble();
+    double media = (nota1+nota2+nota3)/3;
+    if (media>=5.1 && media<=6.9) {
+      System.out.println("Aluno(a): "+ nome + " nota: " + media);
+      System.out.println("Recuperação!");
+    } else if (media <=5) {
+      System.out.println("Reprovado!");
+    }else {
+      System.out.println("Aprovado!");
+    }
+    leitor.close();
+    
+  }
+
+  public static void clearScreen() throws IOException, InterruptedException {
+    try {
+      new ProcessBuilder("cmd", "/c", "cls", "clear").inheritIO().start().waitFor();
+    } catch (Exception e) {
+      System.err.println("Erro ao limpar o terminal: " + e.getMessage());
+    }
+  }
 
 }
