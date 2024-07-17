9f78b9ec62cb2ee88a70e7fc6eb7a7e21bc5f776
Germano-Silva <developer.germano.silva@gmail.com>
2024-07-17 19:53:52 -0300

    Exercício 13

--- Start By Capgemini/02_Trilha_Introdutoria_LOGICA/04_Logica_de_Programacao_e_Algoritmos_3/aulas_java/src/listas/lista_01/Exercicio_13.java
+++ Start By Capgemini/02_Trilha_Introdutoria_LOGICA/04_Logica_de_Programacao_e_Algoritmos_3/aulas_java/src/listas/lista_01/Exercicio_13.java
@@ -6,6 +6,9 @@ package listas.lista_01;
  * Todos os direitos autorais são reservados a Germano Silva.
  */
 
+import java.io.IOException;
+import java.util.Scanner;
+
 /**
  * Exercício: 13.
  * 
@@ -13,5 +16,28 @@ package listas.lista_01;
  */
 
 public class Exercicio_13 {
+    public static void main(String[] args) throws Exception {
+        clearScreen();
+        Scanner leitor = new Scanner(System.in);
+        System.out.print("Informe um número: ");
+        int numero = leitor.nextInt();
+        if (numero<10) {
+            System.out.println("É menor que 10!");
+        } else {
+            System.out.println("É maior que 10!");
+        }
+         
+         
+        leitor.close();
+        
+    }
+
+    public static void clearScreen() throws IOException, InterruptedException {
+        try {
+            new ProcessBuilder("cmd", "/c", "cls", "clear").inheritIO().start().waitFor();
+        } catch (Exception e) {
+            System.err.println("Erro ao limpar o terminal: " + e.getMessage());
+        }
+    }
 
 }
