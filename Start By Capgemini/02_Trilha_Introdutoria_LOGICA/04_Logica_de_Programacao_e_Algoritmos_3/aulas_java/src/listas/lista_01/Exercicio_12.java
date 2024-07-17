fc26ce2a34147547fbce767c0d594f291f20d375
Germano-Silva <developer.germano.silva@gmail.com>
2024-07-17 19:49:00 -0300

    Exercício 12

--- Start By Capgemini/02_Trilha_Introdutoria_LOGICA/04_Logica_de_Programacao_e_Algoritmos_3/aulas_java/src/listas/lista_01/Exercicio_12.java
+++ Start By Capgemini/02_Trilha_Introdutoria_LOGICA/04_Logica_de_Programacao_e_Algoritmos_3/aulas_java/src/listas/lista_01/Exercicio_12.java
@@ -1,5 +1,8 @@
 package listas.lista_01;
 
+import java.io.IOException;
+import java.util.Scanner;
+
 /**
  * Códigos realizados por Germano Silva.
  * GitHub: {@literal https://github.com/Germano-Silva}
@@ -10,23 +13,42 @@ package listas.lista_01;
 /**
  * Exercício: 12.
  * 
- * O custo de um carro novo ao consumidor é a soma do custo de fábrica mais o percentual do distribuidor e dos impostos aplicados 
- * (primeiro os impostos são aplicados sobre o custo de fábrica, e depois o percentual do distribuidor sobre o resultado). Supondo 
- * que o percentual do distribuidor seja de 28% e os impostos 45%, escreva um algoritmo que leia o custo de fábrica de um carro e 
+ * O custo de um carro novo ao consumidor é a soma do custo de fábrica mais o percentual do
+ * distribuidor e dos impostos aplicados (primeiro os impostos são aplicados sobre o custo 
+ * de fábrica, e depois o percentual do distribuidor sobre o resultado). 
+ * Supondo que o percentual do distribuidor seja de 28% e os impostos 45%,
+ * escreva um algoritmo que leia o custo de fábrica de um carro e 
  * informe o custo ao consumidor do mesmo
  */
 public class Exercicio_12 {
-    
+    public static void main(String[] args) throws Exception {
+        clearScreen();
         Scanner leitor = new Scanner(System.in);
 
         System.out.print("Informe o preço de custo do carro para a fabrica: R$ ");
         double custoFabrica = leitor.nextDouble();
-        double imposto = custoFabrica*(45/100);
-        double percentualDistribuidor = custoFabrica*(28/100);
-        System.out.print("São 45% de impostos aplicados no valor de: R$" + imposto );
-        double percentualAcrescimo = leitor.nextDouble();
-        double valorVenda = precoCusto * (1 + percentualAcrescimo / 100);
-        System.out.println("O valor de venda do produto é: R$ " + valorVenda);
+
+        double imposto = custoFabrica * (0.45);
+        System.out.println("São 45% de impostos aplicados no valor de: R$" + imposto );
+
+        double percentualDistribuidor = (custoFabrica + imposto)*(0.28);
+        System.out.println("São 28% de percentual do distribuidor no valor de: R$" + percentualDistribuidor);
+
+
+        double valorTotal = custoFabrica + imposto + percentualDistribuidor;
+        System.out.println("O valor total do produto é de R$ " + valorTotal);
+
         leitor.close();
+        
+    }
+
+    public static void clearScreen() throws IOException, InterruptedException {
+        try {
+                new ProcessBuilder("cmd", "/c", "cls", "clear").inheritIO().start().waitFor();
+        } catch (Exception e) {
+                System.err.println("Erro ao limpar o terminal: " + e.getMessage());
+        }
+    }
+        
 
 }
