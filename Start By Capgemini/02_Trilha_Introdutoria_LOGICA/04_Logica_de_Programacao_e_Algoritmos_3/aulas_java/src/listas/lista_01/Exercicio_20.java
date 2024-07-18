9f1188f8fd8019628667d6e05ecf3196fb5ee787
developer.germano.silva <developer.germano.silva@gmail.com>
2024-07-18 13:04:19 +0000

    Exercício 20

--- Start By Capgemini/02_Trilha_Introdutoria_LOGICA/04_Logica_de_Programacao_e_Algoritmos_3/aulas_java/src/listas/lista_01/Exercicio_20.java
+++ Start By Capgemini/02_Trilha_Introdutoria_LOGICA/04_Logica_de_Programacao_e_Algoritmos_3/aulas_java/src/listas/lista_01/Exercicio_20.java
@@ -17,5 +17,68 @@ package listas.lista_01;
  * “(N) Não”. Informar total de carros com ano até 2000 e total geral.
  */
 public class Exercicio_20 {
+    public static void main(String[] args) throws Exception {
+        Scanner leitor = new Scanner(System.in);
+        double desconto = 0.0;
+        double valorTotal = 0.0;
+        boolean sair = true;
+        String opcao;
+        
+        while (sair) {
+          System.out.println("╔════════════════════════════════════════════╗");
+          System.out.println("║           Bem-vindo ao sistema             ║");
+          System.out.println("║              CARANGO VELHO                 ║");
+          System.out.println("╚════════════════════════════════════════════╝");
+          System.out.println();
+          System.out.println("══════════ Desconto (D) Sair (S) ═════════════");
+          opcao = leitor.next();
+          if(opcao.equals("D")){
+            System.out.println("Informe o nome do veiculo: ");
+            String veiculo = leitor.next();
+            System.out.println("Informe o ano do veiculo: ");
+            int ano = leitor.nextInt();
+            System.out.println("Informe o valor do veiculo: ");
+            double valor = leitor.nextDouble();
+            
+              if (ano>2000) {
+                desconto = valor * 0.07;
+                System.out.println("Veiculo: " + veiculo);
+                System.out.println("Valor do desconto: 7% R$" + desconto);
+                valorTotal = valor - desconto;
+                System.out.println("Valor total do veiculo com desconto: " + valorTotal);
+              } else {
+                desconto = valor * 0.12;
+                System.out.println("Veiculo: " + veiculo);
+                System.out.println("Valor do desconto: 12% R$" + desconto);
+                valorTotal = valor - desconto;
+                System.out.println("Valor total do veiculo com desconto: " + valorTotal);
+                
+              }
+          }else if(opcao.equals("S")){
+            System.out.println();
+            System.out.println("╔════════════════════════════════════════════╗");
+            System.out.println("║           Obrigado por utilizar o          ║");
+            System.out.println("║            sistema CARANGO VELHO           ║");
+            System.out.println("╚════════════════════════════════════════════╝");
+            sair = false;
+          }else{
+            System.out.println();
+            System.out.println("╔════════════════════════════════════════════╗");
+            System.out.println("║                Opção invalida              ║");
+            System.out.println("╚════════════════════════════════════════════╝");
+  
+          }
+        }
+        leitor.close();
+        
+      }
+      
+        public static void clearScreen() throws IOException, InterruptedException {
+          try {
+            new ProcessBuilder("cmd", "/c", "cls", "clear").inheritIO().start().waitFor();
+            } catch (Exception e) {
+              System.err.println("Erro ao limpar o terminal: " + e.getMessage());
+            }
+        }
 
 }
