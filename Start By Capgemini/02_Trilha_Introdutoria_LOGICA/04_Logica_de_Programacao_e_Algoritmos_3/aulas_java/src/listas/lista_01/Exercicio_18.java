3fc1cacf6205ca6b0b191abd5a0f864c4ed98f0d
developer.germano.silva <developer.germano.silva@gmail.com>
2024-07-18 11:25:55 +0000

    Exercício 18

--- Start By Capgemini/02_Trilha_Introdutoria_LOGICA/04_Logica_de_Programacao_e_Algoritmos_3/aulas_java/src/listas/lista_01/Exercicio_18.java
+++ Start By Capgemini/02_Trilha_Introdutoria_LOGICA/04_Logica_de_Programacao_e_Algoritmos_3/aulas_java/src/listas/lista_01/Exercicio_18.java
@@ -13,6 +13,29 @@ package listas.lista_01;
  * Faça um algoritmo que receba a idade de 75 pessoas e mostre mensagem informando “maior de idade” e
  * “menor de idade” para cada pessoa. Considere a idade a partir de 18 anos como maior de idade
  */
-public class Exercicio_18 {
 
+public class Exercicio_18 {
+    public static void main(String[] args) throws Exception {
+        Scanner leitor = new Scanner(System.in);
+
+        for (int i = 1; i>=75 ; i++) {
+          System.out.print("Informe a idade:");
+          int idade = leitor.nextInt();
+          if (idade<18) {
+            System.out.println("Menor de idade!");
+          } else {
+            System.out.println("Maior de idade!");
+          }
+        }
+        leitor.close();
+      
+    }
+    
+      public static void clearScreen() throws IOException, InterruptedException {
+        try {
+          new ProcessBuilder("cmd", "/c", "cls", "clear").inheritIO().start().waitFor();
+          } catch (Exception e) {
+            System.err.println("Erro ao limpar o terminal: " + e.getMessage());
+          }
+      }
 }
