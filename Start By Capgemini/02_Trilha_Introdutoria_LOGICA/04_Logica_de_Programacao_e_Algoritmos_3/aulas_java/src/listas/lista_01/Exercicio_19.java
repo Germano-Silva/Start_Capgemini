1f78ce4e98820dc484fc48937e485e267c10334b
developer.germano.silva <developer.germano.silva@gmail.com>
2024-07-18 11:50:35 +0000

    Exercício 19

--- Start By Capgemini/02_Trilha_Introdutoria_LOGICA/04_Logica_de_Programacao_e_Algoritmos_3/aulas_java/src/listas/lista_01/Exercicio_19.java
+++ Start By Capgemini/02_Trilha_Introdutoria_LOGICA/04_Logica_de_Programacao_e_Algoritmos_3/aulas_java/src/listas/lista_01/Exercicio_19.java
@@ -14,5 +14,44 @@ package listas.lista_01;
  * No final informe total de homens e de mulheres
  */
 public class Exercicio_19 {
+    public static void main(String[] args) throws Exception {
+        Scanner leitor = new Scanner(System.in);
+        int mulher = 0;
+        int homem = 0;
+
+        for (int i = 1; i<=5 ; i++) {
+
+          System.out.println("Informe o nome: ");
+          String nome = leitor.nextLine();
+
+          System.out.print("Informe o seu sexo biologico (M) Macho (F) Femea: ");
+          String sexo = leitor.nextLine();
+
+          if (sexo.equals("M")) {
+            System.out.println("Nome: " + nome);
+            System.out.println("Sexo biologico: Homen ");
+            homem = homem + 1;
+
+          } else if(sexo.equals("F")) {
+            System.out.println("Nome: " + nome);
+            System.out.println("Sexo biologico: Mulher ");
+            mulher = mulher+1;
+          }else{
+            System.out.println("Sexo informado incorretamente!");
+          }
+
+        }
+        System.out.println("A quantidade de mulheres é: " + mulher + " A quantidade de homens é: " + homem);
+        leitor.close();
+      
+    }
+    
+      public static void clearScreen() throws IOException, InterruptedException {
+        try {
+          new ProcessBuilder("cmd", "/c", "cls", "clear").inheritIO().start().waitFor();
+          } catch (Exception e) {
+            System.err.println("Erro ao limpar o terminal: " + e.getMessage());
+          }
+      }
 
 }
