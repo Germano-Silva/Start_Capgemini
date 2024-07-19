5ed4ab2f4a8bfd9dcaf508a014d95d50ca8ecf77
Germano-Silva <developer.germano.silva@gmail.com>
2024-07-19 14:44:21 -0300

    Exercício 35

--- Start By Capgemini/02_Trilha_Introdutoria_LOGICA/04_Logica_de_Programacao_e_Algoritmos_3/aulas_java/src/listas/lista_01/Exercicio_35.java
+++ Start By Capgemini/02_Trilha_Introdutoria_LOGICA/04_Logica_de_Programacao_e_Algoritmos_3/aulas_java/src/listas/lista_01/Exercicio_35.java
@@ -27,10 +27,48 @@ public class Exercicio_35 {
     clearScreen();
     Scanner leitor = new Scanner(System.in);
 
-    
+    System.out.println("Escolha o tipo de cliente:");
+    System.out.println("1 - Residência (R$0,60 por kWh)");
+    System.out.println("2 - Comércio (R$0,48 por kWh)");
+    System.out.println("3 - Indústria (R$1,29 por kWh)");
+    System.out.print("Digite o número correspondente ao tipo de cliente: ");
+    int cliente = leitor.nextInt();
+
+    double valor = 0.0;
+
+    switch (cliente) {
+        case 1:
+            valor = 0.60;
+            break;
+        case 2:
+            valor = 0.48;
+            break;
+        case 3:
+            valor = 1.29;
+            break;
+        default:
+            System.out.println("Opção inválida. Escolha um número de 1 a 3.");
+            break;
+    }
+
+    if (cliente >= 1 && cliente <= 3) {
+        System.out.print("Digite a quantidade de kWh consumidos: ");
+        double consumo = leitor.nextDouble();
+
+        double valorConta = valor * consumo;
+
+        if (cliente==1) {
+          System.out.println("Tipo de Cliente: Residencial.");
+        } else if(cliente==2) {
+          System.out.println("Tipo de Cliente: Comercial.");
+        }else{
+          System.out.println("Tipo de Cliente: Industrial.");
+        }
+        System.out.println("Consumo de kWh: " + consumo);
+        System.out.println("Valor da conta de luz: R$" + valorConta);
+    }
 
     leitor.close();
-    
   }
   
   public static void clearScreen() throws IOException, InterruptedException {
