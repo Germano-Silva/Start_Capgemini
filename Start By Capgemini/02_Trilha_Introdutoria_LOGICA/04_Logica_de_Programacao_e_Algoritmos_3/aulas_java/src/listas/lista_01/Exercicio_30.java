3cbe08b9ce9a6294bcb55c253fdc41284bdefbfc
Germano-Silva <developer.germano.silva@gmail.com>
2024-07-19 10:25:59 -0300

    Exercício 30

--- Start By Capgemini/02_Trilha_Introdutoria_LOGICA/04_Logica_de_Programacao_e_Algoritmos_3/aulas_java/src/listas/lista_01/Exercicio_30.java
+++ Start By Capgemini/02_Trilha_Introdutoria_LOGICA/04_Logica_de_Programacao_e_Algoritmos_3/aulas_java/src/listas/lista_01/Exercicio_30.java
@@ -20,9 +20,54 @@ public class Exercicio_30 {
     public static void main(String[] args) throws Exception {
         clearScreen();
         Scanner leitor = new Scanner(System.in);
-    
+        System.out.println("=======================================================");
+        System.out.println("          Digite três valores inteiros distintos:      ");
+        System.out.println("=======================================================");
+        System.out.println();
+        System.out.print("Valor 1: ");
+        int valor1 = leitor.nextInt();
+        System.out.print("Valor 2: ");
+        int valor2 = leitor.nextInt();
+        System.out.print("Valor 3: ");
+        int valor3 = leitor.nextInt();
+
         
-    
+        int n1, n2, n3;
+
+        
+        if (valor1 <= valor2 && valor1 <= valor3) {
+            n1 = valor1;
+            if (valor2 <= valor3) {
+                n2 = valor2;
+                n3 = valor3;
+            } else {
+                n2 = valor3;
+                n3 = valor2;
+            }
+        } else if (valor2 <= valor1 && valor2 <= valor3) {
+            n1 = valor2;
+            if (valor1 <= valor3) {
+                n2 = valor1;
+                n3 = valor3;
+            } else {
+                n2 = valor3;
+                n3 = valor1;
+            }
+        } else { // valor3 é o n1
+            n1 = valor3;
+            if (valor1 <= valor2) {
+                n2 = valor1;
+                n3 = valor2;
+            } else {
+                n2 = valor2;
+                n3 = valor1;
+            }
+        }
+
+        System.out.println("=======================================================");
+        System.out.println("\n            Valores em ordem crescente:");
+        System.out.println("                "+n1 + ", " + n2 + ", " + n3);
+        System.out.println("=======================================================");
         leitor.close();
         
       }
