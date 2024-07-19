2500d1ec1f7fe6f842047ae9b411c252eb344754
Germano-Silva <developer.germano.silva@gmail.com>
2024-07-19 14:15:40 -0300

    Exercício 33

--- Start By Capgemini/02_Trilha_Introdutoria_LOGICA/04_Logica_de_Programacao_e_Algoritmos_3/aulas_java/src/listas/lista_01/Exercicio_33.java
+++ Start By Capgemini/02_Trilha_Introdutoria_LOGICA/04_Logica_de_Programacao_e_Algoritmos_3/aulas_java/src/listas/lista_01/Exercicio_33.java
@@ -26,8 +26,36 @@ public class Exercicio_33 {
   public static void main(String[] args) throws Exception {
     clearScreen();
     Scanner leitor = new Scanner(System.in);
+    System.out.println("Escolha o nível do professor:");
+    System.out.println("1 - Nível 1 (R$12,00 por hora/aula)");
+    System.out.println("2 - Nível 2 (R$17,00 por hora/aula)");
+    System.out.println("3 - Nível 3 (R$25,00 por hora/aula)");
+    System.out.print("Digite o número correspondente ao nível: ");
+    int nivel = leitor.nextInt();
+    double horaAula = 0.0;
+        
+    switch (nivel) {
+        case 1:
+            horaAula = 12.0;
+            break;
+        case 2:
+            horaAula = 17.0;
+            break;
+        case 3:
+            horaAula = 25.0;
+            break;
+        default:
+            System.out.println("Opção inválida. Escolha um número de 1 a 3.");
+            break;
+    }
+    if (nivel >= 1 && nivel <= 3) {
+        System.out.print("Digite o número de horas trabalhadas: ");
+        int horasTrabalhadas = leitor.nextInt();
 
-    
+        double salario = horaAula * horasTrabalhadas;
+
+        System.out.println("O salário do professor é: R$" + salario);
+    }
 
     leitor.close();
     
