1c5f451f05258059c88dfc60dd732d2fb46b59c7
Germano-Silva <developer.germano.silva@gmail.com>
2024-07-18 21:00:14 -0300

    Exercício 26

--- Start By Capgemini/02_Trilha_Introdutoria_LOGICA/04_Logica_de_Programacao_e_Algoritmos_3/aulas_java/src/listas/lista_01/Exercicio_26.java
+++ Start By Capgemini/02_Trilha_Introdutoria_LOGICA/04_Logica_de_Programacao_e_Algoritmos_3/aulas_java/src/listas/lista_01/Exercicio_26.java
@@ -21,8 +21,28 @@ public class Exercicio_26 {
     public static void main(String[] args) throws Exception {
         clearScreen();
         Scanner leitor = new Scanner(System.in);
-    
+        System.out.println("Informe um número entre 1 e 5");
+        int numero = leitor.nextInt();
         
+        switch (numero) {
+            case 1:
+                System.out.println("Um");
+                break;
+            case 2:
+                System.out.println("Dois");
+                break;
+            case 3:
+                System.out.println("Três");
+                break;
+            case 4:
+                System.out.println("Quatro");
+                break;
+            case 5:
+                System.out.println("Cinco");
+                break;
+            default:
+                System.out.println("Número inválido.");
+        }
     
         leitor.close();
         
