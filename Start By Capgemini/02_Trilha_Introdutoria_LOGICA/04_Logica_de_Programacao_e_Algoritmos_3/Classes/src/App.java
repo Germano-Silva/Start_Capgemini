f41272c795ec80d45080eb8fd27ab53e6bf59df2
Germano-Silva <developer.germano.silva@gmail.com>
2024-07-20 19:10:32 -0300

    POO - Criando objeto

--- Start By Capgemini/02_Trilha_Introdutoria_LOGICA/04_Logica_de_Programacao_e_Algoritmos_3/Classes/src/App.java
+++ Start By Capgemini/02_Trilha_Introdutoria_LOGICA/04_Logica_de_Programacao_e_Algoritmos_3/Classes/src/App.java
@@ -1,5 +1,19 @@
+import java.util.*;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner leitor = new Scanner(System.in);
+        
+        Pessoa objetoPessoa = new Pessoa();
+
+        System.out.println("Digite o Peso da Pessoa:");
+        objetoPessoa.peso = leitor.nextFloat();
+        System.out.println("Digite o Altura da Pessoa:");
+        objetoPessoa.altura = leitor.nextFloat();
+
+        System.out.println("O imc é: " + objetoPessoa.calcularIMC());
+         
+        leitor.close();
+        
     }
 }
--- Start By Capgemini/02_Trilha_Introdutoria_LOGICA/04_Logica_de_Programacao_e_Algoritmos_3/Classes/src/Pessoa.java
+++ Start By Capgemini/02_Trilha_Introdutoria_LOGICA/04_Logica_de_Programacao_e_Algoritmos_3/Classes/src/Pessoa.java
@@ -4,10 +4,11 @@ public class Pessoa {
     float peso;
     float altura;
     
-    
+
     //metodo
     public float calcularIMC(){
         float imc = peso/(altura*altura);
         return imc;
     }
+    
 }
