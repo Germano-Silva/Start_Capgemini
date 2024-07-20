0b2dec67d97dc70d16a35efa084009772846616a
Germano-Silva <developer.germano.silva@gmail.com>
2024-07-20 18:57:49 -0300

    Método IMC

--- Start By Capgemini/02_Trilha_Introdutoria_LOGICA/04_Logica_de_Programacao_e_Algoritmos_3/Classes/src/Pessoa.java
+++ Start By Capgemini/02_Trilha_Introdutoria_LOGICA/04_Logica_de_Programacao_e_Algoritmos_3/Classes/src/Pessoa.java
@@ -3,6 +3,11 @@ public class Pessoa {
     // Atributo
     float peso;
     float altura;
-
-
+    
+    
+    //metodo
+    public float calcularIMC(){
+        float imc = peso/(altura*altura);
+        return imc;
+    }
 }
