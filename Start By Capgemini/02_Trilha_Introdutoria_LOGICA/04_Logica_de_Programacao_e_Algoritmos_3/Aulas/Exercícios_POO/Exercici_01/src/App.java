ebaa16b57afac9038c3bd3d184f26250257064a2
Germano-Silva <developer.germano.silva@gmail.com>
2024-07-21 21:00:13 -0300

    Exercício 01

--- Start By Capgemini/02_Trilha_Introdutoria_LOGICA/04_Logica_de_Programacao_e_Algoritmos_3/Aulas/Exercícios_POO/Exercici_01/src/App.java
+++ Start By Capgemini/02_Trilha_Introdutoria_LOGICA/04_Logica_de_Programacao_e_Algoritmos_3/Aulas/Exercícios_POO/Exercici_01/src/App.java
@@ -1,4 +1,5 @@
-import java.util.Scanner;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 public class App {
     public static void main(String[] args) throws Exception {
@@ -9,15 +10,20 @@ public class App {
         pessoa.setNome(leitor.next());
         System.out.println("Informe a altura:");
         pessoa.setAltura(leitor.nextDouble());
-        System.out.println("Informe a data de nascimento (dd/MM/yyyy):");
-        pessoa.setDataNascimento(leitor.next());
+        leitor.nextLine();
 
+        System.out.println("Informe a data de nascimento (dd/MM/yyyy):");
+        SimpleDateFormat dateFormatada = new SimpleDateFormat("dd/MM/yyyy");
+        String dataNascimentoStr = leitor.nextLine();
+        leitor.nextLine();
+        java.util.Date dataNascimento = dateFormatada.parse(dataNascimentoStr);
+        pessoa.setDataNascimento(dataNascimento);
+    
         leitor.close();
 
         System.out.println("\nDados da Pessoa:");
         System.out.println(pessoa.atributosPessoa());
-
-        // Calculando e imprimindo a idade da pessoa
+        
         int idade = pessoa.calcularIdade();
         System.out.println("Idade: " + idade + " anos");
     }
--- Start By Capgemini/02_Trilha_Introdutoria_LOGICA/04_Logica_de_Programacao_e_Algoritmos_3/Aulas/Exercícios_POO/Exercici_01/src/Pessoa.java
+++ Start By Capgemini/02_Trilha_Introdutoria_LOGICA/04_Logica_de_Programacao_e_Algoritmos_3/Aulas/Exercícios_POO/Exercici_01/src/Pessoa.java
@@ -27,11 +27,17 @@ public class Pessoa {
     public void setAltura(double altura) {
         this.altura = altura;
     }
+
     public int calcularIdade() {
-        int idade;
+        Date dataAtual = new Date();
+        long milissegundosNascimento = dataNascimento.getTime();
+        long milissegundosAtual = dataAtual.getTime();
+        long diferenca = milissegundosAtual - milissegundosNascimento;
+        long idade = diferenca / 1000 / 60 / 60 / 24 / 365;
         
-        return idade;
+        return (int)idade;
     }
+    
     public String atributosPessoa() {
         return "Nome: " + this.nome + "\n" +
                "Data de Nascimento: " + this.dataNascimento + "\n" +
