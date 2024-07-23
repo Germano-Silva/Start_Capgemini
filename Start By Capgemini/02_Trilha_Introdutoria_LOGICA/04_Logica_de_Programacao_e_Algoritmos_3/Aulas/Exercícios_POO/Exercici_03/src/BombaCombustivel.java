1ffdfbfa2764123f4d589f4af9c7762725907c35
Germano-Silva <developer.germano.silva@gmail.com>
2024-07-22 21:24:42 -0300

    Atualização - Exercício 03

--- Start By Capgemini/02_Trilha_Introdutoria_LOGICA/04_Logica_de_Programacao_e_Algoritmos_3/Aulas/Exercícios_POO/Exercici_03/src/BombaCombustivel.java
+++ Start By Capgemini/02_Trilha_Introdutoria_LOGICA/04_Logica_de_Programacao_e_Algoritmos_3/Aulas/Exercícios_POO/Exercici_03/src/BombaCombustivel.java
@@ -2,7 +2,34 @@ public class BombaCombustivel {
     private String tipoCombustível;
     private float valorLitro;
     private float quantidadeCombustível;
+    
+    public BombaCombustivel(String tipoCombustível, float valorLitro, float quantidadeCombustível) {
+        this.tipoCombustível = tipoCombustível;
+        this.valorLitro = valorLitro;
+        this.quantidadeCombustível = quantidadeCombustível;
+    }
+    
+    public String getTipoCombustível() {
+        return tipoCombustível;
+    }
+    public void setTipoCombustível(String tipoCombustível) {
+        this.tipoCombustível = tipoCombustível;
+    }
+    public float getValorLitro() {
+        return valorLitro;
+    }
+    public void setValorLitro(float valorLitro) {
+        this.valorLitro = valorLitro;
+    }
+    public float getQuantidadeCombustível() {
+        return quantidadeCombustível;
+    }
+    public void setQuantidadeCombustível(float quantidadeCombustível) {
+        this.quantidadeCombustível = quantidadeCombustível;
+    }
 
+    
+    
 
 
 }
