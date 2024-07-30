a65660384ad3bb29cdc86a164fabce37d822ebc6
Germano-Silva <developer.germano.silva@gmail.com>
2024-07-29 22:19:46 -0300

    Exercício POO 03

--- Start By Capgemini/02_Trilha_Introdutoria_LOGICA/04_Logica_de_Programacao_e_Algoritmos_3/Aulas/Exercícios_POO/Exercici_03/src/BombaCombustivel.java
+++ Start By Capgemini/02_Trilha_Introdutoria_LOGICA/04_Logica_de_Programacao_e_Algoritmos_3/Aulas/Exercícios_POO/Exercici_03/src/BombaCombustivel.java
@@ -1,19 +1,19 @@
 public class BombaCombustivel {
-    private String tipoCombustível;
+    private String tipoCombustivel;
     private float valorLitro;
-    private float quantidadeCombustível;
+    private float quantidadeCombustivel;
     
-    public BombaCombustivel(String tipoCombustível, float valorLitro, float quantidadeCombustível) {
-        this.tipoCombustível = tipoCombustível;
+    public BombaCombustivel(String tipoCombustivel, float valorLitro, float quantidadeCombustivel) {
+        this.tipoCombustivel = tipoCombustivel;
         this.valorLitro = valorLitro;
-        this.quantidadeCombustível = quantidadeCombustível;
+        this.quantidadeCombustivel = quantidadeCombustivel;
     }
     
-    public String getTipoCombustível() {
-        return tipoCombustível;
+    public String gettipoCombustivel() {
+        return tipoCombustivel;
     }
-    public void setTipoCombustível(String tipoCombustível) {
-        this.tipoCombustível = tipoCombustível;
+    public void settipoCombustivel(String tipoCombustivel) {
+        this.tipoCombustivel = tipoCombustivel;
     }
     public float getValorLitro() {
         return valorLitro;
@@ -21,15 +21,36 @@ public class BombaCombustivel {
     public void setValorLitro(float valorLitro) {
         this.valorLitro = valorLitro;
     }
-    public float getQuantidadeCombustível() {
-        return quantidadeCombustível;
+    public float getquantidadeCombustivel() {
+        return quantidadeCombustivel;
     }
-    public void setQuantidadeCombustível(float quantidadeCombustível) {
-        this.quantidadeCombustível = quantidadeCombustível;
+    public void setquantidadeCombustivel(float quantidadeCombustivel) {
+        this.quantidadeCombustivel = quantidadeCombustivel;
     }
 
-    
-    
+    public void abastecerPorValor(double valor) {
+            double litros = valor / this.valorLitro;
+            System.out.println("Foram abastecidos " + litros + " litros.");
+    }
 
+    public void abastecerPorLitro(double litros) {
+                double valor = litros * valorLitro;
+                System.out.println("O valor a ser pago é R$ " + valor + ".");
+    }
+
+    public void alterarValor(double novoValor) {
+            this.valorLitro = (float) novoValor;
+            System.out.println("O valor do litro foi alterado para R$ " + valorLitro + ".");
+    }
+
+    public void alterarCombustivel(String novoTipo) {
+        this.tipoCombustivel = novoTipo;
+        System.out.println("O tipo de combustível foi alterado para " + this.tipoCombustivel + ".");
+    }
+
+    public void alterarQuantidadeCombustivel(double novaQuantidade) {
+            this.quantidadeCombustivel = (float) novaQuantidade;
+            System.out.println("A quantidade de combustível foi alterada para " + this.quantidadeCombustivel + " litros.");
+    }
 
 }
