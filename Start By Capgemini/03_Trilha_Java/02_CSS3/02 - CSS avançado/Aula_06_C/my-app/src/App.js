37425ec9bfe5463aba459a4e53be19f58530559e
Germano Silva <104945531+Germano-Silva@users.noreply.github.com>
2024-10-22 23:12:56 -0300

    Aula 6.C

--- Start By Capgemini/03_Trilha_Java/02_CSS3/02 - CSS avançado/Aula_06_C/my-app/src/App.js
+++ Start By Capgemini/03_Trilha_Java/02_CSS3/02 - CSS avançado/Aula_06_C/my-app/src/App.js
@@ -1,14 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import styled from 'styled-components';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
         <p>Meu primeiro app react</p>
       </header>
     </div>
