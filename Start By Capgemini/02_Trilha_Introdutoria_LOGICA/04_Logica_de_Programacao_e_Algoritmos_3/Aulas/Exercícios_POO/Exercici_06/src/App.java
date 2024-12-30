97e29ecdf4b2cf617468390123ed805e2ade5865
Germano-Silva <developer.germano.silva@gmail.com>
2024-12-30 13:50:42 -0300

    Atualiza .gitignore para ignorar pastas e arquivos especificados

--- Start By Capgemini/02_Trilha_Introdutoria_LOGICA/04_Logica_de_Programacao_e_Algoritmos_3/Aulas/Exercícios_POO/Exercici_06/src/App.java
+++
@@ -1,6 +0,0 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        Invoice inv = new Invoice(1,"Mouse", -1, 100.0f);
-        System.out.println("Valor total = " + inv.getInvoceAmount());
-    }
-}
